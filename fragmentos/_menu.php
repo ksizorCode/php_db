5592590f70ecd99c98d0887ac6989414db0637b4
Miguel <miguelestebn@gmail.com>
2023-04-27 18:01:29 +0200

    Update _menu.php

--- fragmentos/_menu.php
+++ fragmentos/_menu.php
@@ -1,6 +1,5 @@
 <ul>
     <li><a href="index.php">Inicio</a></li>
     <li><a href="read.php">Leer DB</a></li>
-    <li><a href="add.php">Añadir Datos</a></li>
     <li><a href="install.php">Resetear</a></li>
 </ul>
